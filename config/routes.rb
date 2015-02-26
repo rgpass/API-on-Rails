2b44845345e3474429823a15de3d8352c5a3813b
rgpass <rgpass@gmail.com>
2015-02-26 17:22:38 -0500

    Set route constraints for API

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  
+  # API Definition
+  # See all MIME types accepted by Rails
+    # $ rails c
+    # Mime::SET.collect(&:to_s)
+  # Want to access this by going to api.market_place_api.dev
+  # http://apionrails.icalialabs.com/book/chapter_two#aside-common_api_patterns
+  namespace :api, defaults: { format: :json },
+                  constraints: { subdomain: 'api' }, path: '/' do
+    # We list resources here
+  end
 end
