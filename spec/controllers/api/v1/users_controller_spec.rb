3210277bb4311fa235f754ba15c1dede3bab4479
rgpass <rgpass@gmail.com>
2015-02-27 11:09:14 -0500

    Add users#create endpoint

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -17,4 +17,41 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     it { should respond_with 200 }
   end
 
+  describe 'POST #create' do
+    context 'when successfully created' do
+      before(:each) do
+        @user_attributes = FactoryGirl.attributes_for :user
+        post :create, { user: @user_attributes }, format: :json
+      end
+
+      it 'renders json representation of the user just created' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql @user_attributes[:email]
+      end
+
+      it { should respond_with 201 }
+    end
+
+    context 'when not created' do
+      before(:each) do
+        @invalid_user_attributes = FactoryGirl.attributes_for(:user, email: nil)
+        post :create, { user: @invalid_user_attributes }, format: :json
+      end
+
+      it 'renders an errors json' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it 'renders json errors on why user could not be created' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "can't be blank"
+      end
+
+      # 422 - Unprocessable Entity
+      # Everything is formatted correctly in the request, but could not be processed
+      it { should respond_with 422 }
+    end
+  end
+
 end
