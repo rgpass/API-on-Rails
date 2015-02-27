df13e84d3ebece4a44e8299320b2019691861d9f
rgpass <rgpass@gmail.com>
2015-02-27 11:16:15 -0500

    Add users#update endpoint

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -54,4 +54,38 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     end
   end
 
+  describe 'PUT/PATCH #update' do
+    context 'when successfully updated' do
+      before(:each) do
+        @user = FactoryGirl.create(:user)
+        patch :update, { id: @user.id, user: { email: 'newemail@example.com' } }, format: :json
+      end
+
+      it 'renders json representation of the user just updated' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql 'newemail@example.com'
+      end
+
+      it { should respond_with 200 }
+    end
+
+    context 'when not updated' do
+      before(:each) do
+        @user = FactoryGirl.create(:user)
+        patch :update, { id: @user.id, user: { email: 'bademail.com' } }, format: :json
+      end
+
+      it 'renders an errors json' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it 'renders json errors on why user could not be updated' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include 'is invalid'
+      end
+
+      it { should respond_with 422 }
+    end
+  end
 end
